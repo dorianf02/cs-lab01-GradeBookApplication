74108850b7c5882521cb8b0d74ebbb345f18029e
dorianf02 <dododson12@gmail.com>
2023-04-03 16:42:56 +0200

    test

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
 ﻿
 using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,6 +10,26 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double avgGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Less than 5 students");
+            var scale = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[scale - 1] <= avgGrade)
+                return 'A';
+            else if (grades[(scale * 2) - 1] <= avgGrade)
+                return 'B';
+            else if (grades[(scale * 3) - 1] <= avgGrade)
+                return 'C';
+            else if (grades[(scale * 4) - 1] <= avgGrade)
+                return 'D';
+            else
+                return 'F';
+                return base.GetLetterGrade(avgGrade);
+           
         }
     }
 }
