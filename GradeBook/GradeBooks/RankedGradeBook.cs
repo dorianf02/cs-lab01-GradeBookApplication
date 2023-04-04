ae524657989f783ecb2272675bb46ffab52787f8
dorianf02 <dododson12@gmail.com>
2023-04-04 16:19:58 +0200

    update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,20 +12,20 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
-        public override char GetLetterGrade(double avgGrade)
+        public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Less than 5 students");
+                throw new InvalidOperationException("Ranked grading requires at least 5 students");
             var scale = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (grades[scale - 1] <= avgGrade)
+            if (grades[scale - 1] <= averageGrade)
                 return 'A';
-            else if (grades[(scale * 2) - 1] <= avgGrade)
+            else if (grades[(scale * 2) - 1] <= averageGrade)
                 return 'B';
-            else if (grades[(scale * 3) - 1] <= avgGrade)
+            else if (grades[(scale * 3) - 1] <= averageGrade)
                 return 'C';
-            else if (grades[(scale * 4) - 1] <= avgGrade)
+            else if (grades[(scale * 4) - 1] <= averageGrade)
                 return 'D';
             else
                 return 'F';
